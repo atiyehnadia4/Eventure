d8c0fa0f0330385168f92f6ec8fc33e4b45b6663
Giselle Ruiz <giseller696@gmail.com>
2019-07-25 09:23:09 -0700

    fixed login page

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ the_jinja_env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        login_url= users.create_login_url("/")
-        user = users.get_current_user()
-        if user:
             main_template = the_jinja_env.get_template("template/main.html")
             self.response.write(main_template.render())
-        else:
-            self.redirect("/login")
+
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         login_url= users.create_login_url("/")
+        user = users.get_current_user()
         self.redirect(login_url)
     def post(self):
         pass
